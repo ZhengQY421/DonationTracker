278d81a22ea687d78f787da905caf0cd5e06bb81
Shouen Lee <shouenlee@gmail.com>
2018-08-31 16:20:15 -0400

    Person1 - shouen

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,13 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  String finalString;
+	  for (int i = 2; i < input.length(); i++) {
+	  	finalString += input.charAt(i);
+	  }
+	  finalString += input.charAt(0);
+	  finalString += input.charAt(1);
+	  return finalString;
 	}
 	
 	/**
