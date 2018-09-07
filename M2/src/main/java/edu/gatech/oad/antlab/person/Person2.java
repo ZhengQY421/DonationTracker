2a188351da85cb1b282e16bba5830e71369fd5f2
Oliver Zheng <ZhengQy421@gmail.com>
2018-09-06 23:22:05 -0400

    Fixed indentation

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,1 +1,61 @@
-package edu.gatech.oad.antlab.person;/** *  A simple class for person 2 *  returns their name and a *  modified string  * * @author Bob * @version 1.1 */public class Person2 {    /** Holds the persons real name */    private String name;	 	/**	 * The constructor, takes in the persons	 * name	 * @param pname the person's real name	 */	 public Person2(String pname) {	   name = pname;	 }	/**	 * This method should take the string	 * input and return its characters in	 * random order.	 * given "gtg123b" it should return	 * something like "g3tb1g2".	 *	 * @param input the string to be modified	 * @return the modified string	 */	private String calc(String input) {		char[] characters = input.toCharArray();		for (int i = 0; i<characters.length; i++){            //Generate two random indices		    int index1 = (int) (Math.random()*characters.length);            int index2 = (int) (Math.random()*characters.length);            //Swap the characters at the indices            char first = characters[index1];            characters[index1] = characters[index2];            characters[index2] = first;        }        return new String(characters);	}	/**	 * Return a string rep of this object	 * that varies with an input string	 *	 * @param input the varying string	 * @return the string representing the 	 *         object	 */	public String toString(String input) {	  return name + calc(input);	}}
+package edu.gatech.oad.antlab.person;
+
+/**
+ *  A simple class for person 2
+ *  returns their name and a
+ *  modified string 
+ *
+ * @author Bob
+ * @version 1.1
+ */
+public class Person2 {
+    /** Holds the persons real name */
+    private String name;
+	 	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+	 public Person2(String pname) {
+	   name = pname;
+	 }
+	/**
+	 * This method should take the string
+	 * input and return its characters in
+	 * random order.
+	 * given "gtg123b" it should return
+	 * something like "g3tb1g2".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) {
+
+		char[] characters = input.toCharArray();
+
+		for (int i = 0; i<characters.length; i++){
+
+            //Generate two random indices
+		    int index1 = (int) (Math.random()*characters.length);
+            int index2 = (int) (Math.random()*characters.length);
+
+            //Swap the characters at the indices
+            char first = characters[index1];
+            characters[index1] = characters[index2];
+            characters[index2] = first;
+        }
+
+        return new String(characters);
+	}
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) {
+	  return name + calc(input);
+	}
+}
