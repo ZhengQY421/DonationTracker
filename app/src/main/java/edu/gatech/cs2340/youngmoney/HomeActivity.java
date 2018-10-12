5c24898998d070fb7ff23c922ba98df7327f9388
Zach Panzarino <zachary@panzarino.com>
2018-10-12 03:42:04 -0400

    Add simple location parser

--- app/src/main/java/edu/gatech/cs2340/youngmoney/HomeActivity.java
+++ app/src/main/java/edu/gatech/cs2340/youngmoney/HomeActivity.java
@@ -1,6 +1,7 @@
 package edu.gatech.cs2340.youngmoney;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,10 +14,17 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        final Button button = findViewById(R.id.log_out_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button button1 = findViewById(R.id.log_out_button);
+        button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 logout();
+            }
+        });
+
+        final Button button2 = findViewById(R.id.locations_button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                locations();
             }
         });
     }
@@ -26,23 +34,9 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /*
-    private void readCSVFile() {
-        SimpleModel model = SimpleModel.INSTANCE;
+    private void locations() {
+        Intent intent = new Intent(this, LocationActivity.class);
+        startActivity(intent);
+    }
 
-        try {
-            InputStream is = getResources().openRawResource(R.raw.sample);
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-
-            String line;
-            br.readLine();
-            while ((line = br.readLine()) != null) {
-                Log.d(HomeActivity.TAG, line);
-                String[] tokens = line.split(",");
-                int id = Integer.parseInt(tokens[1]);
-                model.addItem
-            }
-        }
-    }*/
 }
