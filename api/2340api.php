a23b3c1fac3dfd5e04bc9d35af22d9bb6101868a
John Thompson <jthompson4004@gmail.com>
2018-10-31 15:08:52 -0400

    Begin RESTful API

---
+++ api/2340api.php
@@ -0,0 +1,20 @@
+<?php
+    $src = $_POST['src'];
+    $user = $_POST['user'];
+    $pass = $_POST['pass'];
+    $conn = new mysqli("localhost", "root", "M0ney$3x", "youngmoney");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    if (strcmp($src, "locations") == 0) {
+        $sql = "SELECT * FROM locations";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo json_encode($result);
+        } else {
+            echo "0 results";
+        }
+    }
+    $conn->close();
+?>
