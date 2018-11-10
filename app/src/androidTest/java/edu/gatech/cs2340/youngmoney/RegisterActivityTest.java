45a5aac65b0bb10bc8620b2d1993107d6287b814
Zach Panzarino <zachary@panzarino.com>
2018-11-09 21:22:02 -0500

    Add register test (Individual M10)

---
+++ app/src/androidTest/java/edu/gatech/cs2340/youngmoney/RegisterActivityTest.java
@@ -0,0 +1,60 @@
+/**
+ * @author: Oliver Zheng
+ * @decription: Tests LoginActivity
+ */
+package edu.gatech.cs2340.youngmoney;
+
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import edu.gatech.cs2340.youngmoney.activity.HomeActivity;
+import edu.gatech.cs2340.youngmoney.activity.LoginActivity;
+import edu.gatech.cs2340.youngmoney.activity.RegistrationActivity;
+
+
+public class RegisterActivityTest {
+
+    @Rule
+    public ActivityTestRule<RegistrationActivity> mActivityRule = new ActivityTestRule<>(RegistrationActivity.class);
+
+    /**
+     * Tests mainly the attemptLogin method and UserLoginTask of LoginActivity
+     */
+    @Test
+    public void checkRegister() {
+
+        //Test when both fields are empty
+        onView(withId(R.id.SignUpButton)).perform(click());
+        onView(withId(R.id.Username)).check(matches(hasErrorText("This field is required")));
+
+        //Test with name
+        onView(withId(R.id.Name)).perform(typeText("name"), closeSoftKeyboard());
+
+        //Test when no password
+        onView(withId(R.id.Username)).perform(typeText("user"), closeSoftKeyboard());
+        onView(withId(R.id.SignUpButton)).perform(click());
+        onView(withId(R.id.Password)).check(matches(hasErrorText("This password is incorrect")));
+
+        //Test with password
+        onView(withId(R.id.Password)).perform(typeText("word"), closeSoftKeyboard());
+
+        //Test when everything correct
+        onView(withId(R.id.SignUpButton)).perform(click());
+    }
+}
